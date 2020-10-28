24e33cdf864e3a2ecd50babea699089e051ba6ef
pvduc <pvduc@starboardasia.com>
2020-10-28 18:14:16 +0700

    change vi- > vn

--- Src/resources/views/layouts/master.blade.php
+++ Src/resources/views/layouts/master.blade.php
@@ -83,7 +83,7 @@
               </li>
               <li class="nav-item user-panel-custom">
                 <div class="tab">
-                    <a class="tab-locale vi @if (config('app.locale') === 'vi') selected @endif" href="{{ route('locale','vi') }}">VI</a>
+                    <a class="tab-locale vi @if (config('app.locale') === 'vi') selected @endif" href="{{ route('locale','vi') }}">VN</a>
                     <a class="tab-locale en @if (config('app.locale') === 'en') selected @endif" href="{{ route('locale','en') }}">EN</a>
                   </div>
               </li>
