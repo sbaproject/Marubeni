5150a4384c2c130609a7abaa355cdfdff5249ef9
dtnthach <dtnthach@starboardasia.com>
2020-11-15 20:14:55 +0700

    fix css

--- Src/resources/views/components/locale.blade.php
+++ Src/resources/views/components/locale.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item user-panel-custom">
-    <div class="tab">
+    <div class="row lang">
         <a class="tab-locale vi @if (config('app.locale') === 'vi') selected @endif"
             href="{{ route('locale','vi') }}">VN</a>
         <a class="tab-locale en @if (config('app.locale') === 'en') selected @endif"
